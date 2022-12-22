3dc98a19266a384321b45beed9592a8c4451bcef
Rubanov Vasiliy <rubanovas2005@gmail.com>
2022-12-22 15:47:24 +0700

    fix sructure

--- Models/CreateProjectDto.cs
+++ Models/CreateProjectDto.cs
@@ -2,5 +2,9 @@ namespace task_project_service.Models;
 
 public class CreateProjectDto
 {
-    
+    public string Name { get; set; }
+    public string StartDate { get; set; }
+    public string CompleteData { get; set; }
+    public ProjectStatus Status { get; set; }
+    public int Priority { get; set; }
 }
