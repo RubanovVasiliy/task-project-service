6cff15ff83cdcd33e7e03f4413d2607d6e36df5d
Rubanov Vasiliy <rubanovas2005@gmail.com>
2022-12-22 18:19:15 +0700

    fix StartDate & CompleteData data types

--- Models/CreateProjectDto.cs
+++ Models/CreateProjectDto.cs
@@ -3,8 +3,8 @@ namespace task_project_service.Models;
 public class CreateProjectDto
 {
     public string Name { get; set; }
-    public string StartDate { get; set; }
-    public string CompleteData { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime CompleteData { get; set; }
     public ProjectStatus Status { get; set; }
     public int Priority { get; set; }
 }
--- Models/Project.cs
+++ Models/Project.cs
@@ -7,9 +7,9 @@ public class Project
 {
     [Key] public Guid Id { get; set; }
     [Required] public string Name { get; set; }
-    [Required] public string StartDate { get; set; }
-    [Required] public string CompleteData { get; set; }
+    [Required] public DateTime StartDate { get; set; }
+    [Required] public DateTime CompleteData { get; set; }
     [Required] public ProjectStatus Status { get; set; }
     [Required] public int Priority { get; set; }
-    [JsonIgnore] public List<Task> Tasks { get; set; }
+    [JsonIgnore] public List<Task>? Tasks { get; set; }
 }
